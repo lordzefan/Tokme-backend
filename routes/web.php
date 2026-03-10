f1807eb2450a60355392e1aa830aa6806dd42c64
lord zefan <lordzefan@gmail.com>
2026-03-10 21:48:29 +0700

    Tes Commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', function () {
+    return 'Selamat datang di Tokme Backend!';
+});
+
+Route::get('/dashboard', function () {
+    return 'dashboard';
+});
